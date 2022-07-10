340d9bfac1e3bf2e830b07363d64ba08d7960bce
i7p7x0 <i7p7x0@gmail.com>
2022-07-10 20:42:22 +0545

    Completed Section 5 - Rendering Lists & Conditional Content

--- react-complete-guide/src/components/NewExpense/ExpenseForm.jsx
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.jsx
@@ -23,7 +23,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
@@ -31,6 +31,8 @@ const ExpenseForm = (props) => {
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
+
+    props.onCancel();
   };
   return (
     <form onSubmit={submitHandler}>
@@ -65,6 +67,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
